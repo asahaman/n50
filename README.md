6fc57c600876301a583d2d20658b0f9249fd53cd
asahaman <arnab22.iitkgp@gmail.com>
2020-03-12 12:40:53 -0500

    Python script for downloading E. Coli contig assemblies from NCBI

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Py-Wpg
 Platform for assembly, annotation and visualization of bacterial genomes
 
-Exercise 1: A python (or biopython) script to query NCBI to download E coli contigs in fasta format. We download 1000 contigs and calculate N50 sitatistics 
+Exercise 1: A python (or biopython) script to query NCBI to download E coli contigs in fasta format.
+Three arguments need to be specified for running the script: your valid email address, your NCBI api key and number of assemblies you want to download.
+If you dont have an NCBI api key, create an account at https://www.ncbi.nlm.nih.gov/account/ using your email. Once you sign in, go to the top right corner where your email id displayed. Click on it and you will be directed to a new page where you can find your api key. Copy the key to use it as an argument for the script. 
+
+Usage: python fetch_contigs.py "Valid email id" "NCBI api_key" "number of assemblies" 
--- fetch_contigs.py
+++ fetch_contigs.py
@@ -1,36 +1,35 @@
 import sys
 import os
 import urllib.request
-import gzip
-import re
 from Bio import Entrez
 
-def main(email=None, api_key=None):
+def main(email=None, api_key=None, retmax=None):
     Entrez.email = email
     Entrez.api_key = api_key
 
-    handle = Entrez.esearch(db="assembly", term = "E. coli[Organism] AND contig[Assembly Level]", retmax = 1)
+    handle = Entrez.esearch(db="assembly", term = "E. coli[Organism] AND contig[Assembly Level]", retmax = num)
 
     record = Entrez.read(handle)
-
+    assembly_count = 0
     for i in record['IdList']:
         handle1 = Entrez.esummary(db="assembly", id = i)
         record1 = Entrez.read(handle1)
         url = record1['DocumentSummarySet']['DocumentSummary'][0]['FtpPath_GenBank']
         file_name = os.path.basename(url)
         link = os.path.join(url,file_name+'_genomic.fna.gz')
-        print(link)
+        print(f'{file_name}.fna.gz' +" downloaded")
+        assembly_count += 1
         urllib.request.urlretrieve(link, f'{file_name}.fna.gz')
-        with gzip.open(f'{file_name}.fna.gz','r') as f:
-            x = re.search("^>", f)
-            print (x)
-        
+
+    print ("Total {} assemblies downloaded".format(assembly_count))            
+
 if __name__ == "__main__":
     print("In Main")
     email = sys.argv[1]
     api_key = sys.argv[2]
+    num = sys.argv[3]
 
     print(email)
     print(api_key)
 
-    main(email, api_key)
+    main(email, api_key, num)
