9f8169337fc18bc90c6c335cc6eb008008ef378f
asahaman <arnab22.iitkgp@gmail.com>
2020-03-18 12:09:35 -0500

    Initialized a README and another python script. I am scared to delete original contents from exercise-one, so just added lines to check

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Five arguments need to be specified for running the script: your valid email add
 If you dont have an NCBI api key, create an account at https://www.ncbi.nlm.nih.gov/account/ using your email. Once you sign in, go to the top right corner where your email id displayed. Click on it and you will be directed to a new page where you can find your api key. Copy the key to use it as an argument for the script. 
 
 Usage: python fetch_contigs.py "Valid email id" "NCBI api_key" "Genus name" "Species Name" "Number of assemblies" 
+
+Exercise 2: Python script for assessing fasta assemblies and calculating N50 contig length
--- fetch_contigs.py
+++ fetch_contigs.py
@@ -3,6 +3,42 @@ import os
 import urllib.request
 from Bio import Entrez
 
+#I am now heading to exercise two 
+#I have a pseudocode at the moment which I would ideally like to implement in python
+"""
+while reading zipped assembly file line by line:
+	if line matches "^>":
+		word after that (e.g. RKZR02000001.1) = contig_id
+		length_contig_id = 0
+		append contig_id to contig_list
+	else:
+	#or alternatively elif line matches "^[ATGC]":
+		split line by characters (nucleotides)
+		length_contig_id += length of number of characters
+
+for each element in contig_list:
+	print contig_id , length_contig_id
+	append length_contig_id to length_contig_list
+
+
+
+#N50 calculation
+
+sum_contig_lengths = 0
+
+for each element in length_contig_list:
+	sum_contig_lengths += length_contig_id
+
+L50_length = sum_contig_lengths/2
+temp_sum = 0
+reverse sort length_contig_list (decreasing order, longest first)
+
+for n (0..end_of_length_contig_list):
+	temp_sum += length_contig_list[n]
+	if (temp_sum >= L50_length):
+		print "N50 = length_contig_list[n]"
+"""
+
 def main(email = None, api_key = None, genus = None, species = None, retmax = None):
     Entrez.email = email
     Entrez.api_key = api_key
