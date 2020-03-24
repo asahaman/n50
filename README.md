d130d5fac930b071a1660fde0cc804bde5a057cc
asahaman <arnab22.iitkgp@gmail.com>
2020-03-24 11:49:49 -0500

    N50 lengths can now be calculated. Updated REAMDE

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Py-Wpg
 Platform for assembly, annotation and visualization of bacterial genomes
 
-Exercise 1: A python (with embedded biopython) script to query NCBI to download E coli assemblies in fasta format. 
+**Exercise 1**: A python (with embedded biopython) script to query NCBI to download E coli assemblies in fasta format. 
 Five arguments need to be specified for running the script: your valid email address, your NCBI api key, scientific annotation of the organism (genus and species name, e.g. Salmonella enterica) and number of assemblies you want to download.
 
 If you dont have an NCBI api key, create an account at https://www.ncbi.nlm.nih.gov/account/ using your email. Once you sign in, go to the top right corner where your email id displayed. Click on it and you will be directed to a new page where you can find your api key. Copy the key to use it as an argument for the script. 
@@ -9,19 +9,29 @@ If you dont have an NCBI api key, create an account at https://www.ncbi.nlm.nih.
 Usage: python fetch_contigs.py "Valid email id" "NCBI api_key" "Genus name" "Species Name" "Number of assemblies" 
 
 
-Exercise 2: Python script for assessing fasta assemblies and calculating N50 contig length
+**Exercise 2**: Python script for analyzing fasta assemblies and calculating N50 contig length
 
-Given a directory of zipped (.gz) assembly files, the script reads each assembly file and embedded fasta formatted contigs. The script subsequently calculates contig lengths and assembly N50 values. 
+Given a directory of zipped (.gz) assembly files, the script reads each assembly file and embedded fasta formatted contigs. The script subsequently calculates contig lengths and assembly specific N50 values. 
 
 Usage: python calculating_n50_assemblies.py
 
-Output is written to output.txt file which reads something like:
+The program produces two output files. "*summary_concise.txt*" and "*summary_verbose.txt*". *summary_concise.txt* prints assembly name with N50 values:
 
-Opened file GCA_004160775.2_PDT000319866.2.fna.gz
-File has assembly named AAAAOK02 with following contigs and respective lengths:
-	AAAAOK020000001.1       237814
-	AAAAOK020000002.1       174736
-	AAAAOK020000003.1       165434
-	..............................
-	..............................
-Assembly AAAAOK02 has 268 contigs
+N50 length for assembly RSZF02 is 91963   
+N50 length for assembly RTBZ02 is 71579   
+N50 length for assembly RTFI02 is 93615   
+.......................................   
+.......................................   
+
+*summary_verbose.txt* prints file name, assembly name and every contig's name and length in the following form:  
+
+Opened file GCA_004160775.2_PDT000319866.2.fna.gz  
+File has assembly named AAAAOK02 with following contigs and respective lengths:   
+	AAAAOK020000001.1       237814   
+	AAAAOK020000002.1       174736   
+	AAAAOK020000003.1       165434   
+	..............................   
+	..............................   
+Assembly AAAAOK02 has 268 contigs   
+
+This information is printed for each assembly file   
--- calculating_n50_assemblies.py
+++ calculating_n50_assemblies.py
@@ -1,28 +1,66 @@
 import glob
 import re
 import gzip
+import operator
 
+file1 = open("summary_concise.txt","w")
+file2 = open("summary_verbose.txt","w")
+
+#intializing assembly count
 assembly_count = 0
+
+#iterating through each assembly file
 for file in glob.glob('*.gz'):
-    print ("Opened file {}".format(file))
+    file2.write ("Opened file {}\n".format(file))
     assembly_count += 1
     with gzip.open(file, mode = 'rt') as f:
+
+        #initializing contig length dictionary and counting contigs
         contig_length_dict = {}
         contig_count = 0
+
+        #reading assembly file line by line
         for line in f:
+
+            #capturing assembly name. Although this gets updated with every contig header,
+            #it remains same for every file due to the nature of the regex pattern
             assembly = re.findall(r'^>([A-Z]+?0\d).+',line)
             if len(assembly) > 0:
-                assembly_name = assembly[0]    
+                assembly_name = assembly[0] 
+
+            #capturing contig name, initializing contig length to zero, incrementing contig count       
             x = re.findall(r'^>(.+?)\s', line)
             if len(x) > 0:
               contig_name = x[0]
               contig_length = 0
               contig_count += 1
+            
+            #if the program doesn't find a contig header, the seqeunce length of every line gets added
+            #and the contig length gets updated within the contig length dictionary
             else:
               contig_length += len(line) - 1
               contig_length_dict[contig_name] = contig_length
-        print("File has assembly named {} with following contigs and respective lengths:".format(assembly_name))
-        for key in contig_length_dict:
-            print("\t",key,"\t",contig_length_dict[key])
-        print("Assembly {} has {} contigs\n".format(assembly_name,contig_count))    
 
+        file2.write("File has assembly named {} with following contigs and respective lengths:\n".format(assembly_name))
+        
+        #The assembly length is calculated by summing all contig lengths
+        #The length of every contig from the contig length dictionary is written to a file
+        total_assembly_len = 0
+        for key in contig_length_dict:
+            file2.write("\t{}\t{}\n".format(key,contig_length_dict[key]))
+            total_assembly_len += contig_length_dict[key]
+        file2.write("Assembly {} has {} contigs\n\n".format(assembly_name,contig_count))
+        
+        #L50 length is calculated
+        l50_len = total_assembly_len/2
+        
+        #contig length dictionary is sorted by values to an ordered list of tuples
+        rev_sorted_contig_length_dict = sorted(contig_length_dict.items(), key=operator.itemgetter(1),reverse=True)
+        temp_sum = 0
+        
+        #iterate through decreasing contig lengths and breaking the loop once N50 length is calculated
+        for item in rev_sorted_contig_length_dict:
+            temp_sum += item[1]
+            if temp_sum >= l50_len: 
+                file1.write("N50 length for assembly {} is {}\n".format(assembly_name, item[1]))
+                break
