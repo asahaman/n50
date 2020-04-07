6a2198048f4a72c868641f0e66ba51e0f262ceb3
asahaman <arnab22.iitkgp@gmail.com>
2020-04-07 12:28:18 -0500

    more files

--- setup.py
+++ setup.py
@@ -8,12 +8,20 @@ setup(name='n50',
       version='0.9',
       description='N50 calculation from bacterial assemblies',
       long_description=readme(),
+      classifiers=[
+        'Development Status :: 1 - Planning'
+        'License :: OSI Approved :: Academic Free License (AFL)'
+        'Programming Language :: Python :: 3.6'
+        'Topic :: Text Processing :: General'
+        'Operating System :: Unix'
+      ],
       url='https://github.com/asahaman/n50.git',
       author='Arnab Saha Mandal',
       author_email='arnab.sahamandal@umanitoba.ca',
       license='MIT',
       packages=['n50'],
+      scripts=['bin/n50-assembly'],
       install_requires=[
-          'Bio','markdown'
+          'biopython','markdown'
       ],
       zip_safe=False)
