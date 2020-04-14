4617937778d20a9d7b3e29c3a286218006a8f7ce
asahaman <arnab22.iitkgp@gmail.com>
2020-04-14 11:52:12 -0500

    Version changed, changelog added

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='n50',
-      version='1.0',
+      version='0.1.0',
       description='N50 calculation from bacterial assemblies',
       long_description=readme(),
       classifiers=[
