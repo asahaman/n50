033ddca531463e818a5547f9c95ebf578a75143b
asahaman <arnab22.iitkgp@gmail.com>
2020-04-29 15:36:05 -0500

    tests added

--- n50/calculating_n50_assemblies.py
+++ n50/calculating_n50_assemblies.py
@@ -47,11 +47,20 @@ def contigs_len_calc(open_file = None, contig_dict_ok = None):
             contig = x.group(1)
             contig_dict[contig] = 0
         elif y != None:
-            contig_dict[contig] += len(y[0].rstrip('\n'))
+            try:
+                contig_dict[contig] += len(y[0].rstrip('\n'))
+            except UnboundLocalError:
+                '''
+                This is a case of orphaned nucleotide sequences
+                without a legit > fasta header. Those sequences
+                will be ignored and not added to contig dictionary
+                '''
+                pass
     for key, value in contig_dict.items():
         if value != 0:
             contig_dict_ok[key] = value
-    return contig_dict_ok
+    if contig_dict_ok:
+        return contig_dict_ok
 
 def assembly_len_calc(in_dir = None, f_name_list = None):
     ass_len_list = []
@@ -61,10 +70,12 @@ def assembly_len_calc(in_dir = None, f_name_list = None):
         contig_dict_ok = {}
         if (array_f[-1] == 'gz'):
             with gzip.open(file, mode = 'rt') as open_file:
-                ass_len_list.append(contigs_len_calc(open_file, contig_dict_ok)) 
+                if contigs_len_calc(open_file, contig_dict_ok):
+                    ass_len_list.append(contigs_len_calc(open_file, contig_dict_ok)) 
         else:
             with open(file, mode = 'r') as open_file:
-                ass_len_list.append(contigs_len_calc(open_file, contig_dict_ok))
+                if contigs_len_calc(open_file, contig_dict_ok):
+                    ass_len_list.append(contigs_len_calc(open_file, contig_dict_ok))
     return ass_len_list
             
 def n50_calc(ass_len_list = None):
@@ -86,19 +97,31 @@ def n50_calc(ass_len_list = None):
     return n50_array, n50_array_log
 
 def n50_summary(n50_array = None, n50_array_log = None, out_dir = None, genus = None, species= None):
+    
     output_file = open(os.path.join(out_dir,'summary_statistics.txt'),'w')
     s = pd.Series(n50_array)
+    summary_list = [ 
+        s.count(), 
+        s.min(), s.max(), 
+        round(s.mean(),3),
+        s.median(), 
+        round(s.std(),3)
+    ]
+    
     output_file.write("Number of assemblies is {}\n".format(s.count()))
     output_file.write("Min and Max N50 values are {} and {}\n".format(s.min(),s.max()))
-    output_file.write("Mean of N50 distribution is {:.3f}\n".format(s.mean()))
+    output_file.write("Mean of N50 distribution is {}\n".format(round(s.mean(),3)))
     output_file.write("Median of N50 distribution is {}\n".format(s.median()))
-    output_file.write("Standard deviation of N50 distribution is {:.3f}\n".format(s.std()))
-    plt.hist(n50_array_log, bins = 100, color='green')
-    plt.xlabel('Log (base 10) N50 values')
-    plt.ylabel('Counts')
-    plt.title('Histogram of '+ genus + ' '+ species + ' assembly lengths')
-    plt.savefig(os.path.join(out_dir,'hist.pdf'))
-    return True
+    output_file.write("Standard deviation of N50 distribution is {}\n".format(round(s.std(),3)))
+
+    if plt.hist(n50_array_log, bins = 100, color='green'):
+        plt.xlabel('Log (base 10) N50 values')
+        plt.ylabel('Counts')
+        plt.title('Histogram of '+ genus + ' '+ species + ' assembly lengths')
+        plt.savefig(os.path.join(out_dir,'hist.pdf'))
+        summary_list.append('histogram '+ genus + ' ' + species + ' generated')
+     
+    return summary_list
 
 def main():
     if len(sys.argv) != 5:
@@ -119,28 +142,23 @@ Need to pass 4 arguments corresponding to input directory containing fasta assem
             if fasta_extn_check(in_dir):
                 print("Input directory has fasta files. We can proceed...")
                 file_list = fasta_extn_check(in_dir)
-                if file_list:
-                    ass_len_list = assembly_len_calc(in_dir,file_list)
-                    if ass_len_list:
-                        print("Some (or all) fasta assemblies have non-zero nucleotide sequences")
-                        arr1, arr2 = n50_calc(ass_len_list)
-                        if (arr1, arr2):
-                            print("n50 lengths of assemblies have been calculated")
-                            if output_dir_check(out_dir):
-                                print ("Output dir exists, files can be written")
-                            else:
-                                print ("Output dir does not exist. new created...")
-
-                            n50_summary(arr1, arr2, out_dir, genus, species)
-
-                            if n50_summary(arr1, arr2, out_dir, genus, species):
-                                print ("Hooray! you have calculated n50 summary statistics and plotted a histogram..")
-                            else:
-                                print ("n50 values have been calculated but statistics cant be computed")
-                        else:
-                            sys.exit("Something is wrong with the fasta files...exiting..")
+                print(file_list)
+                ass_len_list = assembly_len_calc(in_dir,file_list)
+                if ass_len_list:
+                    print(ass_len_list)
+                    print("At least one fasta assembly has valid header/s and non-zero nucleotide sequences")
+                    arr1, arr2 = n50_calc(ass_len_list)
+                    print(arr1, arr2)
+                    print("n50 lengths of assemblies have been calculated")
+                    if output_dir_check(out_dir):
+                        print ("Output dir exists, files can be written")
                     else:
-                        sys.exit("The fasta files may not have nucleotide sequences of non-zero lengths")
+                        print ("Output dir does not exist. new created...")
+                    n50_summary(arr1, arr2, out_dir, genus, species)
+                    print(n50_summary(arr1, arr2, out_dir, genus, species))
+                    print ("Hooray! you have calculated n50 summary statistics and plotted a histogram..")
+                else:
+                    sys.exit("No fasta file is valid. Absent headers or nucleoties..exiting..")
             else:
                 sys.exit("There isnt any fasta file in input directory to work with..exiting..")
         else: 
--- tests/test_calculating_n50.py
+++ tests/test_calculating_n50.py
@@ -7,25 +7,53 @@ import os
 import gzip
 import numpy as np
 import pytest
-import pathlib
 
 START_DIR = os.getcwd()
-TEST_INPUT_DIR = START_DIR+"/data"
-FAKE_INPUT_DIR = START_DIR+"/does_not_exist"
+TEST_INPUT_DIR = START_DIR+'/data'
+FAKE_INPUT_DIR = START_DIR+'/does_not_exist'
 EMPTY_DIR = TEST_INPUT_DIR+'/empty_dir'
 
-list1 = ['test_assem1.fasta','test_assem2.fasta','test_assem3.fasta','test_assem4.fasta','test_assem5.fasta']
-list2 = [
-    {'contig1': 8, 'contig2': 5, 'contig3': 4, 'contig4': 1, 'contig5': 7, 'contig6': 2, 'contig7': 3, 'contig8': 10, 'contig9': 6, 'contig10': 9},
-    {'contig1': 1, 'contig2': 2, 'contig3': 3, 'contig4': 4, 'contig5': 5},
-    {'contig1': 8, 'contig2': 5, 'contig3': 150},
-    {'contig2': 5, 'contig3': 150, 'contig4': 12},
-    {'contig2': 5, 'contig3': 150}
+'''
+Different examples of test fasta assembly files have been created in folder data:
+1. test_assem1.fasta - 10 contigs, valid file
+2. test_assem2.fasta - 5 contigs, valid file
+3. test_assem3.fasta - 6 valid contig headers:
+                            2 have valid sequences, 
+                            1 has mix and match of valid sequence and invalid sequence
+                            3 are invalid contigs:
+                                1 has no sequence
+                                1 has blanks
+                                1 has random text paragraph
+4. test_assem4.fasta - 3 invalid contigs:
+                            1 has no sequence
+                            1 has blanks
+                            1 has random text paragraph
+5. test_assem5.fasta - Random text paragraph but will pass as a valid assembly due to the nature of embedded text
+6. test_assem6.fasta - Random text paragraph which won't pass
+7. test_assem7.fasta - Orphaned nucleotides (valid sequences without fasta headers). This won't pass                            
+'''
+
+test_file_list = [
+    'test_assem1.fasta','test_assem2.fasta','test_assem3.fasta',
+    'test_assem4.fasta','test_assem5.fasta','test_assem6.fasta', 
+    'test_assem7.fasta'
 ]
-tup1 = (
-    [7, 4, 150, 150, 150], 
-    [0.845, 0.602, 2.176, 2.176, 2.176]
+
+test_ass_list = [
+    {'contig1': 8, 'contig2': 5, 'contig3': 4, 'contig4': 1, 'contig5': 7, 
+    'contig6': 2, 'contig7': 3, 'contig8': 10, 'contig9': 6, 'contig10': 9}, 
+    {'contig1': 1, 'contig2': 2, 'contig3': 3, 'contig4': 4, 'contig5': 5}, 
+    {'contig1': 5, 'contig2': 150, 'contig3': 12}, 
+    {'I': 29}
+]
+
+test_n50_tuple = (
+    [7, 4, 150, 29],
+    [0.845, 0.602, 2.176, 1.462]
 )
+
+test_summary_list = [4, 4, 150, 47.5, 18.0, 69.236, 'histogram desired organism generated']
+
 def test_input_dir_check_true():
     expected_result = True
     method_result = input_dir_check(TEST_INPUT_DIR)
@@ -48,22 +76,35 @@ def test_input_dir_empty_check_false():
 
 def test_fasta_extn_check_true():
     true_file_list = []
-    for i in range(1,6):
+    for i in range(1,8):
         filename = 'test_assem' + str(i) + '.fasta'
         true_file_list.append(filename)
-    expected_list1 = true_file_list
-    expected_list2 = ['this','is','not','a','file','list']
     method_list = fasta_extn_check(TEST_INPUT_DIR)
-    assert expected_list1 == method_list
-    assert expected_list2 != method_list
+    assert true_file_list == method_list
 
-def test_assembly_len_calc():
-    expected_assembly =  list2
-    method_assembly = assembly_len_calc(TEST_INPUT_DIR,list1)
+def test_fasta_extn_check_false():
+    false_file_list = ['this','is','not','a','file','list']
+    method_list = fasta_extn_check(TEST_INPUT_DIR)
+    assert false_file_list != method_list
+
+def test_assembly_len_calc_true():
+    expected_assembly = test_ass_list
+    method_assembly = assembly_len_calc(TEST_INPUT_DIR,test_ass_list)
     assert expected_assembly == method_assembly
 
+def test_assembly_len_calc_false():
+    test_ass_list_false = test_ass_list[1:]
+    method_assembly = assembly_len_calc(TEST_INPUT_DIR,test_ass_list)
+    assert test_ass_list_false != method_assembly
+
 def test_n50_calc():
-    expected_tuple = tup1
-    method_tuple = n50_calc(list2)
-    assert expected_tuple == method_tuple    
+    expected_tuple = test_n50_tuple
+    method_tuple = n50_calc(test_ass_list)
+    assert expected_tuple == method_tuple
+print(n50_summary(test_n50_tuple[0],test_n50_tuple[1],TEST_INPUT_DIR,'A','Histogram'))
+
+def test_n50_summary():
+    expected_list = test_summary_list
+    method_list = n50_summary(test_n50_tuple[0],test_n50_tuple[1],TEST_INPUT_DIR,'desired','organism')
+    assert expected_list == method_list
 
